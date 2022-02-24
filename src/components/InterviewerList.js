597a2e5024fe6151d92c036764475478b9778bd3
Chris Tate <Chris_tate_94@hotmail.com>
2022-02-24 09:58:08 -0500

    added new features

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,10 +2,6 @@ import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "styles/variables.scss";
 
-// interviewers:array - an array of objects as seen above
-// setInterviewer:function - a function that accepts an interviewer id. This function will simply be passed down to the InterviewerListItem
-// interviewer:number - a number that represents the id of the currently selected interviewer
-
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map((interviewer) => {
     return (
@@ -13,8 +9,8 @@ export default function InterviewerList(props) {
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });
